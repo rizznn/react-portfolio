9a6df0210353d8983481d1875b1a570f68f53f32
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-19 03:19:41 -0400

    contact section'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import About from './components/About';
-import './App.css';
+// import Navigation from './components/Navigation'
+import ContactForm from './components/Contact';
 
 function App() {
+  // const [categories] = useState([
+  //   {
+  //     name: 'commercial',
+  //     description: 'Photos of grocery stores, food trucks, and other commercial projects',
+  //   },
+  //   { name: 'portraits', description: 'Portraits of people in my life' },
+  //   { name: 'food', description: 'Delicious delicacies' },
+  //   { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  // ]);
+
+  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  // const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
+      {/* <Navigation
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Navigation> */}
       <main>
-
+      <main>
+        <ContactForm></ContactForm>
 
         <About></About>
+      </main>
       </main>
     </div>
   );
