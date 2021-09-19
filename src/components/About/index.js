eefb3b45e082c07f4a7b5e5584c98a2413cf5576
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-18 22:38:27 -0400

    about me added

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function About() {
+    const [pages] = useState([
+        {
+            name: "about me"
+        }
+    ]);
+    const [currentPage] = useState(pages[0]);
+
+    return (
+        <section className="text-center justify-center container">
+            <h1 className="about m-5">{capitalizeFirstLetter(currentPage.name)}</h1>
+            <div>
+                <p>I am a student in University of Toronto taking a coding bootcamp course. 
+                The experience I gained during the bootcamp helps me build my skills as I learned the latest technologies taught by skilled and experienced professionals.
+                It also enhanced my time management skills as I face challenge every week with tight deadlines while working full time.
+                I learned a lot working with different people collaborating in making professional applications with he latest technologies.</p>
+            </div>
+        </section>
+
+    );
+}
+
+export default About;
